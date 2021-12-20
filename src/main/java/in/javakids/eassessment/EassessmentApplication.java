76869f7427c9e5cc8ed9f8fd96ce4966cb0558f1
shashi <devwork276@gmail.com>
2021-12-20 23:02:49 +0530

    initial commit

---
+++ src/main/java/in/javakids/eassessment/EassessmentApplication.java
@@ -0,0 +1,13 @@
+package in.javakids.eassessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EassessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EassessmentApplication.class, args);
+	}
+
+}

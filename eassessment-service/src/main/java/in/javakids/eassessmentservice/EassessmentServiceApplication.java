e915795ebf31783d09d726987bea777b48024b2e
shashi <devwork276@gmail.com>
2021-12-20 23:18:53 +0530

    Added files

---
+++ eassessment-service/src/main/java/in/javakids/eassessmentservice/EassessmentServiceApplication.java
@@ -0,0 +1,14 @@
+package in.javakids.eassessmentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EassessmentServiceApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(EassessmentServiceApplication.class, args);
+    }
+
+}
